e04101c111c76499af4a4ba124bc06e3ad25661d
user <mr.sergei82@mail.ru>
2025-06-16 00:19:13 +0300

    2

--- user/src/main/java/com/example/user/model/User.java
+++ user/src/main/java/com/example/user/model/User.java
@@ -1,10 +1,8 @@
 package com.example.user.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+@Table(name = "users")
 @Entity
 public class User {
     @Id
