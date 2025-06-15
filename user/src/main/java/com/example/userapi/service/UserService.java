651025bfe2394d477f2b0f0eb8f1484092a20d57
user <mr.sergei82@mail.ru>
2025-06-15 13:00:07 +0300

    GET and PUT in user

---
+++ user/src/main/java/com/example/userapi/service/UserService.java
@@ -0,0 +1,31 @@
+package com.example.userapi.service;
+
+import com.example.userapi.model.User;
+import com.example.userapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User createOrUpdateUser(User user) {
+        return userRepository.save(user);
+    }
+}
