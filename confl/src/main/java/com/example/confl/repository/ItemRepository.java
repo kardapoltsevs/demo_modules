26621340d56c99075256b260ec1ebc5013e92a0f
user <mr.sergei82@mail.ru>
2025-06-21 12:51:57 +0300

    ItemService.java

--- confl/src/main/java/com/example/confl/repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.confl.repository;
-
-import com.example.confl.model.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-    List<Item> findByNameContaining(String name);
-}
--- user/src/main/java/com/example/user/controller/UserController.java
+++ user/src/main/java/com/example/user/controller/UserController.java
@@ -2,24 +2,24 @@ package com.example.user.controller;
 
 import com.example.user.model.User;
 import com.example.user.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
-    @Autowired
+
+
     private UserRepository userRepository;
 
-    // GET метод - получение всех пользователей
     @GetMapping
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
-    // POST метод - создание нового пользователя
     @PostMapping
     public User createUser(@RequestBody User user) {
         return userRepository.save(user);
